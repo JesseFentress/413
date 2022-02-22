bf4590af1bb9cb24fa0ceadcf9116242d3989c2a
JesseFentress <jessefentress246@gmail.com>
2022-02-21 19:19:24 -0500

    array and ll done

---
+++ Lab4/PQDriver.java
@@ -0,0 +1,42 @@
+public class PQDriver {
+    public static void main(String[] args) {
+       /* PriorityQueueLinkedList<String>  pqll = new PriorityQueueLinkedList<String>("ok");
+        System.out.println(pqll.size());
+        pqll.insert("penis", 123);
+        System.out.println(pqll.peek());
+        System.out.println(pqll.size());
+        System.out.println("remove" + pqll.remove());
+        pqll.insert("cock", 7);
+        System.out.println(pqll.peek());
+        System.out.println(pqll.size());
+        pqll.insert("balls", 3004);
+        System.out.println(pqll.peek());
+        System.out.println(pqll.size());
+        pqll.insert("anus", 4989);
+        System.out.println(pqll.peek());
+        pqll.changePriority("anus", 30);
+        System.out.println(pqll.peek());
+        System.out.println(pqll.size());
+        System.out.println("remove" + pqll.remove());
+        System.out.println("remove" + pqll.remove());
+        System.out.println(pqll.peek());
+        System.out.println(pqll.size());
+        System.out.println("remove" + pqll.remove());
+        System.out.println("remove" + pqll.remove());*/
+
+        /*PriorityQueueArray pqa = new PriorityQueueArray("max");
+        System.out.println(pqa.size());
+        System.out.println(pqa.peek());
+        pqa.insert("ok", 30);
+        System.out.println(pqa.peek());
+        pqa.insert("im", 1);
+        System.out.println(pqa.peek());
+        pqa.insert("not", 12);
+        System.out.println(pqa.peek());
+        pqa.insert("sure", 45);
+        System.out.println(pqa.remove());
+        System.out.println(pqa.remove());
+        System.out.println(pqa.peek());
+        System.out.println(pqa.size());*/
+    }
+}
