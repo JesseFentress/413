ecdb9127c359cc45a093eb423dfee9b1a640e479
JesseFentress <jessefentress246@gmail.com>
2022-02-24 18:00:33 -0500

    asda

--- Lab4/PQDriver.java
+++ Lab4/PQDriver.java
@@ -1,30 +1,7 @@
 public class PQDriver {
     public static void main(String[] args) {
-       /* PriorityQueueLinkedList<String>  pqll = new PriorityQueueLinkedList<String>("ok");
-        System.out.println(pqll.size());
-        pqll.insert("penis", 123);
-        System.out.println(pqll.peek());
-        System.out.println(pqll.size());
-        System.out.println("remove" + pqll.remove());
-        pqll.insert("cock", 7);
-        System.out.println(pqll.peek());
-        System.out.println(pqll.size());
-        pqll.insert("balls", 3004);
-        System.out.println(pqll.peek());
-        System.out.println(pqll.size());
-        pqll.insert("anus", 4989);
-        System.out.println(pqll.peek());
-        pqll.changePriority("anus", 30);
-        System.out.println(pqll.peek());
-        System.out.println(pqll.size());
-        System.out.println("remove" + pqll.remove());
-        System.out.println("remove" + pqll.remove());
-        System.out.println(pqll.peek());
-        System.out.println(pqll.size());
-        System.out.println("remove" + pqll.remove());
-        System.out.println("remove" + pqll.remove());*/
-
-        System.out.println("PQ with Array");
+       
+        /*System.out.println("PQ with Array");
         PriorityQueueArray pqa = new PriorityQueueArray();
         pqa.insert("22", 22);
         pqa.insert("360", 360);
@@ -92,7 +69,68 @@ public class PQDriver {
             if (pqh.heapsort()[i][0] != null) {
                 System.out.print("[" + pqh.heapsort()[i][0] + " " +  pqh.heapsort()[i][1] + "]" + " ");
             }
-        }
+        }*/
+
+        /*System.out.println("PQ with LL");
+        PriorityQueueLinkedListMin<Integer> pqll = new PriorityQueueLinkedListMin<Integer>();
+        pqll.insert(67, 67);
+        pqll.insert(98, 98);
+        pqll.insert(14, 14);
+        pqll.insert(86, 86);
+        pqll.print();
+        System.out.println();
+        System.out.println("Insert 23: ");
+        pqll.insert(23, 23);
+        pqll.print();
+        System.out.println();
+        System.out.println("Remove: ");
+        pqll.remove();
+        pqll.print();
+        System.out.println();
+        System.out.println("Change Priority (86 -> 37): ");
+        pqll.changePriority(86, 37);
+        pqll.print();
+        System.out.println();*/
+
+        /*System.out.println("PQ with Heap");
+        PriorityQueueHeapMin<Integer> pqh = new PriorityQueueHeapMin<Integer>();
+        pqh.insert(22, 22);
+        pqh.insert(360, 360);
+        pqh.insert(17, 17);
+        pqh.insert(66, 66);
+        pqh.print();
+        System.out.println();
+        System.out.println("Insert 43: ");
+        pqh.insert(43, 43);
+        pqh.print();
+        System.out.println();
+        System.out.println("Remove: ");
+        pqh.remove();
+        pqh.print();
+        System.out.println();
+        System.out.println("Change priority (360 -> 41): ");
+        pqh.changePriority(360, 41);
+        pqh.print();  */
+
+        /*PriorityQueueArrayMin pqa = new PriorityQueueArrayMin();
+        pqa.insert("22", 22);
+        pqa.insert("360", 360);
+        pqa.insert("17", 17);
+        pqa.insert("66", 66);
+        pqa.print();
+        System.out.println();
+        System.out.println("Insert 53: ");
+        pqa.insert("53", 53);
+        pqa.print();
+        System.out.println();
+        System.out.println("Remove: ");
+        pqa.remove();
+        pqa.print();
+        System.out.println();
+        System.out.println("Change priority (66 -> 39): ");
+        //pqa.changePriority();
+        pqa.print();
+        System.out.println();*/
         
     }
 }
